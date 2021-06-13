2ba1f854ad3ccbe2feba3a08eaed1a0957511152
Mihir Sawant <mihir9552@gmail.com>
2021-06-12 23:30:49 -0400

    update subjects.sql

--- etls/subjects.sql
+++ etls/subjects.sql
@@ -1,7 +1,8 @@
-SELECT a.subject_id
+SELECT distinct a.subject_id
 from `physionet-data.mimiciii_clinical.admissions` as a
 join `physionet-data.mimiciii_clinical.patients` as p on a.subject_id=p.subject_id
-
+join `physionet-data.mimiciii_clinical.microbiologyevents` as m on a.SUBJECT_ID=m.SUBJECT_ID
+join `physionet-data.mimiciii_clinical.prescriptions` as pres on a.SUBJECT_ID=pres.SUBJECT_ID
 join (
     select subject_id, sum(num_hours) as total_hours
     from
@@ -11,5 +12,12 @@ join (
     having sum(num_hours) > 7
     and sum(num_hours) < 2000
 ) x on a.subject_id=x.subject_id
+join (
+    select drug from `eco-rune-309016.mimic_iii_data.drug_names`
+) y on pres.DRUG=y.drug
 and TIMESTAMP_DIFF(a.ADMITTIME, p.DOB, DAY) > 18*365
-and TIMESTAMP_DIFF(a.ADMITTIME, p.DOB, DAY) < 120*365;
+and TIMESTAMP_DIFF(a.ADMITTIME, p.DOB, DAY) < 120*365
+and (m.SPEC_TYPE_DESC like '%BLOOD CULTURE%'
+or m.SPEC_TYPE_DESC like '%URINE%'
+or m.SPEC_TYPE_DESC like '%STOOL%'
+or m.SPEC_TYPE_DESC like '%CSF%');
